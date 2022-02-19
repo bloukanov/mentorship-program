75df7cffdf0ac6f0beefb14c3df70ca7d070ba68
Bogdan <loukanob@mskcc.org>
2022-02-19 17:57:19 -0500

    implemented gale-shapley if too many pairs

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ import math
 start_all = dt.datetime.now()
 
 df_interests = pd.read_csv('interests.csv')
-df_mentees = pd.read_csv('mentees3.csv')
-df_mentors = pd.read_csv('mentors3.csv')
+df_mentees = pd.read_csv('mentees2.csv')
+df_mentors = pd.read_csv('mentors2.csv')
 
 mentees = df_mentees.name.unique()
 mentors = df_mentors.name.unique()
@@ -48,65 +48,111 @@ print('finished calcualting '+str(len(pairs_scores))+' scores. time elapsed',dt.
 
 # choose the pairings that result in the greatest total score
 
+# can enumerate all possibilities only if 10 or fewer pairs can be made, because
+# possibilities = (n_pairs)!. 10! = 3,628,800
+if n_pairs <= 10:
 
-start_com = dt.datetime.now()
-print('enumerating '+format(math.factorial(n_pairs),',d')+' possibilities',start_com)
+    start_com = dt.datetime.now()
+    print('enumerating '+format(math.factorial(n_pairs),',d')+' possibilities',start_com)
 
-# pairs_sets = list(itertools.combinations(poss_pairs,n_pairs)) # blows up way too quickly
+    # pairs_sets = list(itertools.combinations(poss_pairs,n_pairs)) # blows up way too quickly
 
-# for i in range(len(other_group)**len(limiting_group)) # also blows up way too much
+    # for i in range(len(other_group)**len(limiting_group)) # also blows up way too much
 
-# doesn't matter which group is Combinations and which is Permutations. If smaller group is
-# C, then it is n_pairsCn_pairs, which is just the group itself.
-permutations = list(itertools.permutations(iter(other_group),n_pairs))
+    # doesn't matter which group is Combinations and which is Permutations. If smaller group is
+    # C, then it is n_pairsCn_pairs, which is just the group itself.
+    permutations = list(itertools.permutations(iter(other_group),n_pairs))
 
-print('done. time elapsed',dt.datetime.now() - start_com)
+    print('done. time elapsed',dt.datetime.now() - start_com)
+            
+
+    start_final = dt.datetime.now()
+    print('calculating totals and exporting top 1M rows',start_final)
+
+    totals = []
+    pairings_strings = []
+    for p in permutations:
+        total = 0
+        string = ''
+        for i in range(n_pairs):
+            pair = limiting_group[i]+'_'+p[i]
+            total += pairs_scores[pair]
+            string += '__' + pair
+        totals.append(total)
+        pairings_strings.append(string)
+
+    final_df = pd.DataFrame({'pairings':pairings_strings,'score':totals}).sort_values(by='score',ascending=False).iloc[:1000000]
+
+    final_df.to_csv('result.csv',index=False)
+    print('done. time elapsed', dt.datetime.now() - start_final)
+
+    print('all done. time elapsed',dt.datetime.now() - start_all)
+
+else:
+## GALE SHAPLEY THEOREM
+
+    from collections import Counter
+    from copy import copy
+
+    man_list = list(limiting_group)
+    women_list = list(other_group)
+
+    # women_df = pd.DataFrame({'A': [3,4,2,1], 'B': [3,1,4,2], 'C':[2,3,4,1], 'D':[3,2,1,4]})
+    # women_df.index = man_list
+
+    # man_df = pd.DataFrame({'A': [1,1,2,4], 'B': [2,4,1,2], 'C':[3,3,3,3], 'D':[4,2,4,1]})
+    # man_df.index = man_list
+
+    scores_df = pd.DataFrame(np.nan,index=man_list,columns=women_list)
+    for i in limiting_group:
+        for j in other_group:
+            scores_df.loc[i,j] = pairs_scores[i+'_'+j]
+
+    # dict to control which women each man can make proposals
+    women_available = {man:women_list for man in man_list}
+    # waiting list of men that were able to create pair on each iteration
+    waiting_list = []
+    # dict to store created pairs
+    proposals = {}
+    # variable to count number of iterations
+    count = 0
+
+    # while not all men have pairs
+    while len(waiting_list)<len(man_list):
+        print(count)
+        # man makes proposals
+        for man in man_list:
+            if man not in waiting_list:
+                # each man make proposal to the top women from it's list
+                women = women_available[man]
+                # best_choice = man_df.loc[man][man_df.loc[man].index.isin(women)].idxmin()
+                best_choice = scores_df.loc[man][scores_df.loc[man].index.isin(women)].idxmax()
+                # proposals[(man, best_choice)]=(man_df.loc[man][best_choice],
+                #                                     women_df.loc[man][best_choice])
+                proposals[(man, best_choice)] = scores_df.loc[man,best_choice]
+        # if women have more than one proposals 
+        # she will choose the best option
+        overlays = Counter([key[1] for key in proposals.keys()])
+        # cycle to choose the best options
+        for women in overlays.keys():
+            if overlays[women]>1:
+                # pairs to drop from proposals
+                pairs_to_drop = sorted({pair: proposals[pair] for pair in proposals.keys() 
+                        if women in pair}.items(), 
+                    # key=lambda x: x[1][1]
+                    key = lambda x: x[1],
+                    reverse = True
+                    )[1:]
+                # if man was rejected by woman
+                # there is no pint for him to make proposal 
+                # second time to the same woman
+                for p_to_drop in pairs_to_drop:
+                    del proposals[p_to_drop[0]]
+                    _women = copy(women_available[p_to_drop[0][0]])
+                    _women.remove(p_to_drop[0][1])
+                    women_available[p_to_drop[0][0]] = _women
+        # man who successfully created pairs must be added to the waiting list 
+        waiting_list = [man[0] for man in proposals.keys()]
+        # update counter
+        count+=1
         
-
-start_final = dt.datetime.now()
-print('calculating totals and exporting top 1M rows',start_final)
-
-totals = []
-pairings_strings = []
-for p in permutations:
-    total = 0
-    string = ''
-    for i in range(n_pairs):
-        pair = limiting_group[i]+'_'+p[i]
-        total += pairs_scores[pair]
-        string += '__' + pair
-    totals.append(total)
-    pairings_strings.append(string)
-
-final_df = pd.DataFrame({'pairings':pairings_strings,'score':totals}).sort_values(by='score',ascending=False).iloc[:1000000]
-
-final_df.to_csv('result.csv',index=False)
-print('done. time elapsed', dt.datetime.now() - start_final)
-
-print('all done. time elapsed',dt.datetime.now() - start_all)
-
-
-
-
-
-# start_paring = dt.datetime.now()
-# print('paring down to eligible combinations',start_paring)
-# final = []
-# for i in pairs_sets:
-#     firsts = []
-#     seconds = []
-#     for j in range(n_pairs):
-#         pair = i[j]
-#         under = pair.find('_')
-#         if pair[0:under] not in firsts:
-#             firsts.append(pair[0:under])
-#         else:
-#             break
-#         if pair[(under+1):len(pair)] not in seconds:
-#             seconds.append(pair[(under+1):len(pair)])
-#         else:
-#             break
-#     if len(firsts) == n_pairs and len(seconds) == n_pairs:
-#         final.append([a[0] + '_' + a[1] for a in list(zip(firsts,seconds))])
-
-# print('pared down to '+str(len(final))+' eligible pairings',dt.datetime.now() - start_paring)
