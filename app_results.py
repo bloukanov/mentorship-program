0a3accadb526a185a6ac9475096a3bc17c947e58
Bogdan <loukanob@mskcc.org>
2022-03-20 18:52:58 -0400

    fixed presenting team info

--- app_results.py
+++ app_results.py
@@ -39,6 +39,7 @@ with col1:
     _Years of experience in role anywhere_: {user_profile.years_all}
     '''
     )
+    st.markdown("_Team_: "+user_profile.team)
 
     st.subheader('Tracks')
 
@@ -49,9 +50,6 @@ with col1:
             
     else:
         st.write('  \n'.join([str(int(user_ints.iloc[n,0])) + ': ' + user_ints.iloc[n,1] for n in range(user_ints.shape[0])]))
-
-    if "Peer Mentorship" in user_ints['interest']:
-        st.write("Team: "+user_profile.team)
 
 
 with col2:
@@ -74,6 +72,7 @@ with col2:
     _Years of experience in role anywhere_: {match_profile.years_all}
     '''
     )
+    st.markdown("_Team_: "+match_profile.team)
 
     st.subheader('Tracks')
 
@@ -85,8 +84,6 @@ with col2:
     else:
         st.write('  \n'.join([str(int(match_ints.iloc[n,0])) + ': ' + match_ints.iloc[n,1] for n in range(match_ints.shape[0])]))
 
-    if "Peer Mentorship" in match_ints['interest']:
-        st.write("Team: "+match_profile.team)
 
 
 ### TODO: add yes/no button
